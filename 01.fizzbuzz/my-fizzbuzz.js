0714bc5ec3c972f3b3a6acf5983b9666f7e99cf9
maimux2x <m0smile10often@gmail.com>
2022-10-18 12:54:24 +0900

    FizzBuzz問題(JavaScript)の実装

---
+++ 01.fizzbuzz/my-fizzbuzz.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function fizzbuzz(num) {
+  if (num % 15 == 0) {
+    return "FizzBuzz";
+  } else if (num % 3 == 0) {
+    return "Fizz";
+  } else if (num % 5 == 0) {
+    return "Buzz";
+  } else {
+    return num;
+  }
+}
+
+let i = 1;
+while (i <= 20) {
+  console.log(fizzbuzz(i));
+  i += 1;
+}

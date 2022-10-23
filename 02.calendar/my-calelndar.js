d129b7637dbf8a792120dc8ff299fa0882b785ae
maimux2x <m0smile10often@gmail.com>
2022-10-23 19:08:54 +0900

    カレンダーのプログラム(JavaScript)課題作成

---
+++ 02.calendar/my-calelndar.js
@@ -0,0 +1,50 @@
+"use strict";
+const { DateTime } = require("luxon");
+const minimist = require("minimist");
+
+const options = minimist(process.argv.slice(2), {
+  year: "y",
+  month: "m",
+  default: {
+    year: DateTime.now().year,
+    month: DateTime.now().month,
+  },
+});
+
+const year = options.y;
+const month = options.m;
+
+const dt = DateTime.fromObject({
+  year: year,
+  month: month,
+  day: 1,
+});
+
+if (dt.month < 10) {
+  console.log(`      ${dt.month}月 ${dt.year}`);
+} else {
+  console.log(`     ${dt.month}月 ${dt.year}`);
+}
+
+console.log("日 月 火 水 木 金 土");
+const endDt = dt.endOf("month");
+
+if (dt.weekday < 7) {
+  const RawSpace = " ";
+  const MakingSpace = RawSpace.repeat(3 * dt.weekday);
+
+  process.stdout.write(MakingSpace);
+}
+
+let newDt = dt;
+while (newDt <= endDt) {
+  process.stdout.write(`${String(newDt.day).padStart(2, " ")} `);
+
+  if (newDt.weekdayShort == "土") {
+    process.stdout.write("\n");
+  }
+
+  newDt = newDt.plus({ day: 1 });
+}
+
+console.log("\n");

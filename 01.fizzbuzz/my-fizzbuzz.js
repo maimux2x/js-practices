a88994a745d265db941f4eaae39bbaecf7d05a11
maimux2x <m0smile10often@gmail.com>
2022-10-24 17:46:57 +0900

    レビューいただいた内容の修正

--- 01.fizzbuzz/my-fizzbuzz.js
+++ 01.fizzbuzz/my-fizzbuzz.js
@@ -1,11 +1,11 @@
 "use strict";
 
 function fizzbuzz(num) {
-  if (num % 15 == 0) {
+  if (num % 15 === 0) {
     return "FizzBuzz";
-  } else if (num % 3 == 0) {
+  } else if (num % 3 === 0) {
     return "Fizz";
-  } else if (num % 5 == 0) {
+  } else if (num % 5 === 0) {
     return "Buzz";
   } else {
     return num;
